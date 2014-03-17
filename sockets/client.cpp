ccac850cad5afc460d42585dc3808d499c8788b9
Jaideep <jaideep.bethu@gmail.com>
2014-03-17 17:04:40 -0400

    Added few changes, allowing multiple clients contacting server

--- sockets/client.cpp
+++ sockets/client.cpp
@@ -7,16 +7,18 @@
 #include<netdb.h>
 #include<errno.h>
 #include<stdio.h>
+#include<sstream>
 
-#define PORT "34567"
+#define PORT "34603"
 
 using namespace std;
 
 int main() {
 	struct addrinfo hint,*res;
 	int sockfd,status;
-	char buff[256];
-
+	char buff[256],outbuff[256];
+	ostringstream oss;
+	memset(&outbuff,'\0',256);
 	memset(&hint,0,sizeof(struct addrinfo));
 	hint.ai_family = AF_UNSPEC;
 	hint.ai_socktype = SOCK_STREAM;
@@ -27,11 +29,30 @@ int main() {
 	}
 
 	sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-	connect(sockfd,res->ai_addr,res->ai_addrlen);
 	
-	recv(sockfd,&buff,sizeof(buff),0);
+	cout<<"client: sockid:"<<sockfd<<endl;
 
-	cout<<buff;
+	if(sockfd==-1) {
+		cout<<"Error: socket \n";
+		exit(1);
+	}
+	int count=0;
+	if(connect(sockfd,res->ai_addr,res->ai_addrlen)!=-1) {
+			
+		while ((status = recv(sockfd,&buff,sizeof(buff),0) !=0 ) && count<10) {
+			count++;
+			cout<<buff;
+			snprintf(outbuff,sizeof(outbuff),"%d\n",count);
+			send(sockfd,outbuff,strlen(outbuff),0);
+			memset(buff,0,sizeof(buff));
+			oss.clear();
+		}
+	}
+	else 
+		cout<<"Error: connect \n";
+	
+	close(sockfd);
+	
 	return 0;
 }
 
--- sockets/server.cpp
+++ sockets/server.cpp
@@ -8,7 +8,7 @@
 #include<errno.h>
 #include<stdio.h>
 
-#define MYPORT "34567"
+#define MYPORT "34603"
 #define BACKLOG 10
 
 using namespace std;
@@ -18,32 +18,57 @@ int main() {
 	struct sockaddr_storage inaddr;
 	socklen_t addr_size;
 	int sockfd,infd,status;
+	char buff[256];
 
+	memset(&buff,'\0',256);
 	memset(&hints,0,sizeof(struct addrinfo));
 
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-//	hints.ai_flags	= AI_PASSIVE;
+	hints.ai_flags	= AI_PASSIVE;
 
-	if((status=getaddrinfo("127.0.0.1",MYPORT,&hints,&res)!=0)) {
-		cout<<"getaddrinfo error\n";
+	if((status=getaddrinfo(NULL,MYPORT,&hints,&res)!=0)) {
+		cout<<"Error: getaddrinfo \n";
 		exit(1);
 	}	
 
 	sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);	
+	cout<<"server: sockid:"<<sockfd<<endl;
 	if(sockfd==-1) {
 		cout<<"Error: socket \n";
 		exit(1);
 	}
 
 	bind(sockfd,res->ai_addr,res->ai_addrlen);
+	while(1) {	
+		listen(sockfd,BACKLOG);
 
-	listen(sockfd,BACKLOG);
+		infd = accept(sockfd,(struct sockaddr*)&inaddr,&addr_size);
+		cout<<"sock:"<<infd<<endl;
 
-	infd = accept(sockfd,(struct sockaddr*)&inaddr,&addr_size);
+		do {
+			status = send(infd,"Hello\n",6,0);  
+			if(status!=-1)
+			{
+				status =recv(infd,&buff,sizeof(buff),0);
+				if(status==0 )
+					break;
+				else if(status==-1) {
+					cout<<"Error:Unable to receive \n";
+					break;
+				}
+				else {
+					cout<<buff<<endl;
+					memset(&buff,'\0',256);
+				}
+			}
+		}while(status!=-1 && status !=0);
+		
+		close(infd);	
+	}
+	cout<<"Out of loop\n";
 	
-	if(send(infd,"Hello\n",6,0) == -1)
-		perror("send");	
-
+	close(sockfd);
+	
 	return 0;
 }
