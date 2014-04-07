66f1bac8208b02bda5ef3690e409ecf6a7d1358e
Jaideep <jaideep.bethu@gmail.com>
2014-04-07 17:04:42 -0400

    learned pipes,fifo,semaphores and wrote some code 4/7:ofc

---
+++ threads/pthread_ex_sem.cpp
@@ -0,0 +1,46 @@
+
+#include<iostream>
+#include<pthread.h>
+#include<stdio.h>
+#include<semaphore.h>
+
+using namespace std;
+long wat = 0;
+sem_t sem;
+
+void * print_message_func(void *ptr) {
+	int *msg;
+	msg = (int*)ptr;
+	sem_wait(&sem);
+	cout<<pthread_self()<<":HERE "<<wat<<endl;
+	for(long i=0;i<1000000;i++) {
+		wat = wat+ 1;
+	}
+	cout<<pthread_self()<<":End "<<wat<<endl;
+	
+	sem_post(&sem);	
+}
+
+int main() {
+	pthread_t th1,th2,th3;
+	int msg1[2]={1,2};
+	int msg2[2]={3,4};
+	int msg3[2]={5,6};
+	int iret1,iret2,iret3;
+
+	sem_init(&sem,0,2);
+
+	iret1 = pthread_create(&th1,NULL,print_message_func, (void*)msg1);
+	iret2 = pthread_create(&th2,NULL,print_message_func, (void*)msg2);
+	iret3 = pthread_create(&th3,NULL,print_message_func, (void*)msg3);
+	cout<<th1<<"  "<<th2<<"  "<<th3<<endl;
+	pthread_join(th1,NULL);
+	cout<<"Over 1"<<endl;
+	pthread_join(th2,NULL);
+	cout<<"Over 2"<<endl;
+	pthread_join(th3,NULL);
+	cout<<"Over 3"<<endl;
+
+	sem_destroy(&sem);
+	return 0;
+}
